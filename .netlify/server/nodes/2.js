e497afbeec364288fb1ca57e6e982fa7dd4b00db
Vanvirsinh <deoravanvirsinh7773@gmail.com>
2023-09-16 23:53:30 +0530

    Dev form submission 1.0.4

--- .netlify/server/nodes/2.js
+++ .netlify/server/nodes/2.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/+page.js";
-export const imports = ["_app/immutable/nodes/2.30297fc7.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.aef4e382.js","_app/immutable/chunks/index.4200373c.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/ContactIcons.340affc3.js","_app/immutable/chunks/index.b970fcc4.js"];
+export const imports = ["_app/immutable/nodes/2.c305da71.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.aef4e382.js","_app/immutable/chunks/index.4200373c.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/ContactIcons.340affc3.js","_app/immutable/chunks/index.b970fcc4.js"];
 export const stylesheets = ["_app/immutable/assets/2.d3b18c49.css"];
 export const fonts = [];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -597,6 +597,24 @@
     </div>
 </section>
 
+<form name="contact" method="POST" data-netlify="true">
+    <p>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+    </p>
+    <p>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+    </p>
+    <p>
+        <label for="message">Message</label>
+        <textarea id="message" name="message" />
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+</form>
+
 <!-- Other Absolute Components -->
 <BackToTop pathName="/" />
 <ContactIcons />
